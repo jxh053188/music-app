27804ff01772175333c9159b7d4c4d09785a1814
jxh0531 <jarred.harkness@gmail.com>
2022-06-17 10:29:35 +0200

    change signup

---
+++ prisma/migrations/20220616135948_update_songs/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `title` on the `Song` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[name]` on the table `Artist` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `duration` to the `Song` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `name` to the `Song` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `url` to the `Song` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Song" DROP COLUMN "title",
+ADD COLUMN     "duration" INTEGER NOT NULL,
+ADD COLUMN     "name" TEXT NOT NULL,
+ADD COLUMN     "url" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Artist_name_key" ON "Artist"("name");
